0438208d07185b107c670f5aad551e2f1f01b8cc
Charlotte Pierce <charlotte@malformed-bits.com>
2021-12-02 16:20:12 +1100

    Completed part 2.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- day_02/main.py
+++ day_02/main.py
@@ -1,17 +1,19 @@
 horizontal = 0
 depth = 0
+aim = 0
 
 f = open("input.txt")
 
 for line in f:
     direction, amount = line.strip().split(" ")
 
-    if direction == "forward":
-        horizontal += int(amount)
-    elif direction == "down":
-        depth += int(amount)
+    if direction == "down":
+        aim += int(amount)
     elif direction == "up":
-        depth -= int(amount)
+        aim -= int(amount)
+    elif direction == "forward":
+        horizontal += int(amount)
+        depth += aim * int(amount)
 
 answer = horizontal * depth
 print(answer)
