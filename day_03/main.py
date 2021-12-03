23b5c5abe2b167565ed2a36262579a9fc9d7e9e0
Charlotte Pierce <charlotte@malformed-bits.com>
2021-12-03 16:36:37 +1100

    Completed part 2.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- day_03/main.py
+++ day_03/main.py
@@ -1,5 +1,4 @@
 def part_one(data):
-
     gamma_rate = ""
 
     for x in range(len(data[0])):
@@ -11,11 +10,43 @@ def part_one(data):
 
     gamma_rate = int(gamma_rate, 2)
     epsilon_rate = int(epsilon_rate, 2)
-    print(gamma_rate)
-    print(epsilon_rate)
-
     power_consumption = gamma_rate * epsilon_rate
     print(power_consumption)
+
+
+def filter(data, pos, ox=True):
+    all_bits_in_pos_x = [line[pos] for line in data]
+    num_ones = all_bits_in_pos_x.count("1")
+    num_zeroes = all_bits_in_pos_x.count("0")
+
+    num_to_keep = "0" if num_zeroes > num_ones else "1"
+    if not ox: num_to_keep = "0" if num_to_keep == "1" else "1"
+
+    data = [line for line in data if line[pos] == num_to_keep]
+
+    return data
+
+
+def part_two(data):
+    oxygen_generator_rating = []
+    c02_scrubber_rating = []
+    data_ox = data
+    data_c02 = data
+    for x in range(len(data[0])):
+        data_ox = filter(data_ox, x)
+        data_c02 = filter(data_c02, x, False)
+
+        if len(oxygen_generator_rating) == 0 and len(data_ox) == 1:
+            oxygen_generator_rating = data_ox[0]
+
+        if len(c02_scrubber_rating) == 0 and len(data_c02) == 1:
+            c02_scrubber_rating = data_c02[0]
+
+    oxygen_generator_rating = int(''.join(oxygen_generator_rating), 2)
+    c02_scrubber_rating = int(''.join(c02_scrubber_rating), 2)
+
+    life_support_rating = oxygen_generator_rating * c02_scrubber_rating
+    print(life_support_rating)
 
 
 f = open("input.txt")
@@ -23,3 +54,4 @@ data = [[c for c in line.strip()] for line in f]
 f.close()
 
 part_one(data)
+part_two(data)
