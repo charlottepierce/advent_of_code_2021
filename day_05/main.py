857f372f0c0aaeeaf4be0c8fbbe352254d3d30fa
Charlotte Pierce <charlotte@malformed-bits.com>
2021-12-05 17:45:28 +1100

    Completed part 2.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- day_05/main.py
+++ day_05/main.py
@@ -1,33 +1,53 @@
+from collections import Counter
+
 def points_on_line(startx, starty, endx, endy, diagonals=False):
-    all_points = []
     if (startx != endx) and (starty != endy) and (not diagonals): return []
 
-    x = min(startx, endx)
-    while x <= max(startx, endx):
+    all_points = []
+    if startx == endx:
         y = min(starty, endy)
         while y <= max(starty, endy):
-            all_points.append((x, y))
+            all_points.append((startx, y))
             y += 1
-        x += 1
+    elif starty == endy:
+        x = min(startx, endx)
+        while x <= max(startx, endx):
+            all_points.append((x, starty))
+            x += 1
+    else:
+        x = startx
+        y = starty
+        while (x != endx) and (y != endy):
+            all_points.append((x, y))
+            if startx < endx: x += 1
+            else: x -= 1
+
+            if starty < endy: y += 1
+            else: y -= 1
+        all_points.append((endx, endy))
+
     return all_points
 
 
 if __name__ == "__main__":
     f = open("input.txt")
 
-    seen_points = []
-    doubly_seen_points = []
+    all_points = []
     for line in f:
         print(line)
         points = line.strip().split(" -> ")
         startx, starty = [int(p) for p in points[0].split(",")]
         endx, endy = [int(p) for p in points[1].split(",")]
-        for p in points_on_line(startx, starty, endx, endy):
-            if p in seen_points and p not in doubly_seen_points:
-                doubly_seen_points.append(p)
-            else:
-                seen_points.append(p)
 
-    print(len(doubly_seen_points))
+        # all_points.extend(points_on_line(startx, starty, endx, endy)) # part one
+        all_points.extend(points_on_line(startx, starty, endx, endy, diagonals=True)) # part two
+
+    counts = Counter(all_points)
+    num_duplicates = 0
+    for c in counts.values():
+        if c > 1:
+            num_duplicates += 1
+
+    print(num_duplicates)
 
     f.close()
