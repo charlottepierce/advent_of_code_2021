898c2e7175619597ac6f7de138b53843974c41d5
Charlotte Pierce <charlotte@malformed-bits.com>
2021-12-09 17:10:42 +1100

    Completed part 2.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- day_09/main.py
+++ day_09/main.py
@@ -1,24 +1,71 @@
+def is_low_point(row, col, height_map):
+    height = height_map[row][col]
+    lowest_point = True
+    # up
+    if (row > 0) and (height_map[row - 1][col] <= height):
+        lowest_point = False
+    # down
+    if ((row + 1) < len(height_map)) and (height_map[row + 1][col] <= height):
+        lowest_point = False
+    # left
+    if (col > 0) and (height_map[row][col - 1] <= height):
+        lowest_point = False
+    # right
+    if ((col + 1) < len(height_map[0])) and (height_map[row][col + 1] <= height):
+        lowest_point = False
+
+    return lowest_point
+
+
+def flood_fill(row, col, height_map, seen):
+    if (row, col) in seen: return 0, seen
+    if height_map[row][col] == 9: return 0, seen
+
+    seen.append((row, col))
+    size = 1
+    # up
+    if row > 0:
+        additional_size, seen = flood_fill(row - 1, col, height_map, seen)
+        size += additional_size
+    # down
+    if (row + 1) < len(height_map):
+        additional_size, seen = flood_fill(row + 1, col, height_map, seen)
+        size += additional_size
+    # left
+    if (col > 0):
+        additional_size, seen = flood_fill(row, col - 1, height_map, seen)
+        size += additional_size
+    # right
+    if (col + 1) < len(height_map[0]):
+        additional_size, seen = flood_fill(row, col + 1, height_map, seen)
+        size += additional_size
+
+    return size, seen
+
+
+def part_two(height_map):
+    basin_sizes = []
+
+    seen = []
+    for row in range(len(height_map)):
+        for col in range(len(height_map[0])):
+            if is_low_point(row, col, height_map):
+                basin_size, seen = flood_fill(row, col, height_map, seen)
+                basin_sizes.append(basin_size)
+
+    three_largest_basin_sizes = sorted(basin_sizes)[-3:]
+    result = 1
+    for x in three_largest_basin_sizes:
+        result *= x
+    return result
+
+
 def part_one(height_map):
     low_points = []
     for row in range(len(height_map)):
         for col in range(len(height_map[0])):
-            height = height_map[row][col]
-            lowest_point = True
-            # up
-            if (row > 0) and (height_map[row-1][col] <= height):
-                lowest_point = False
-            # down
-            if ((row + 1) < len(height_map)) and (height_map[row+1][col] <= height):
-                lowest_point = False
-            # left
-            if (col > 0) and (height_map[row][col-1] <= height):
-                lowest_point = False
-            # right
-            if ((col + 1) < len(height_map[0])) and (height_map[row][col+1] <= height):
-                lowest_point = False
-
-            if lowest_point:
-                low_points.append(height)
+            if is_low_point(row, col, height_map):
+                low_points.append(height_map[row][col])
 
     return sum([x + 1 for x in low_points])
 
@@ -31,4 +78,5 @@ if __name__ == "__main__":
         height_map.append(heights)
     f.close()
 
-    print(part_one(height_map))
+    # print(part_one(height_map))
+    print(part_two(height_map))
