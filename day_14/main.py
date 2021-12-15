e7e5fa950c650bfe51d093d5c2b5e691db5bd87e
Charlotte Pierce <charlotte@malformed-bits.com>
2021-12-15 15:24:45 +1100

    Completed part 2.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- day_14/main.py
+++ day_14/main.py
@@ -1,23 +1,43 @@
-def run_step(template, insertion_rules):
-    new_template = template[0]
+import math
+
+
+def run_step(pairs, insertion_rules):
+    new_pairs = {}
+    for p in pairs.keys():
+        inserted = insertion_rules[p]
+        new_pair1 = p[0] + inserted
+        new_pair2 = inserted + p[1]
+
+        new_pairs[new_pair1] = new_pairs.get(new_pair1, 0) + 1 * pairs[p]
+        new_pairs[new_pair2] = new_pairs.get(new_pair2, 0) + 1 * pairs[p]
+
+    return new_pairs
+
+
+def run_logic(template, insertion_rules, num_steps):
+    pair_counts = {}
     for i in range(len(template) - 1):
         pair = template[i:i+2]
-        new_template += insertion_rules[pair] + pair[1]
+        pair_counts[pair] = 1
 
-    return new_template
+    for step in range(num_steps):
+        pair_counts = run_step(pair_counts, insertion_rules)
 
+    element_counts = {}
+    for pair in pair_counts.keys():
+        element1, element2 = [c for c in pair]
+        element_counts[element1] = element_counts.get(element1, 0) + pair_counts[pair]
+        element_counts[element2] = element_counts.get(element2, 0) + pair_counts[pair]
 
-def part_one(template, insertion_rules):
-    for step in range(10):
-        template = run_step(template, insertion_rules)
+    for element in element_counts.keys():
+        element_counts[element] = math.floor(element_counts[element] / 2)
 
-    elements = set([x for x in template])
-    counts = {}
-    for element in elements:
-        counts[element] = template.count(element)
+    element_counts[template[0]] += 1
+    element_counts[template[-1]] += 1
 
-    counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
-    answer = counts[0][1] - counts[-1][1]
+    element_counts = sorted(element_counts.items(), key=lambda x: x[1], reverse=True)
+
+    answer = element_counts[0][1] - element_counts[-1][1]
     return answer
 
 
@@ -34,4 +54,5 @@ if __name__ == "__main__":
 
     f.close()
 
-    print(part_one(template, insertion_rules))
+    # print(run_logic(template, insertion_rules, 10))
+    print(run_logic(template, insertion_rules, 40))
